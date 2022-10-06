c617f9019df3862ce8a8ca5bbed7ad6f031ab8f2
Torres <charlie.torres@arcadis.com>
2022-10-06 16:14:21 +0100

    Identical Copy of checks

--- jobs/app.py
+++ jobs/app.py
@@ -20,13 +20,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
-    if connection != None:
+    if connection is not None:
         connection.close()
 
     
